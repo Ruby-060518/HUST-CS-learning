cdcf2ebc6efea423f5a9b20109743d10c320294a
Ruby <2452538750@qq.com>
2025-12-08 23:12:17 +0800

    DEBUG

--- Mobile/新人任务/第五期/MyApplicationsurvey/app/src/main/java/com/example/myapplicationsurvey/Navigation/Screens.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplicationsurvey.Navigation
-
-
-sealed class Screen(val route: String) {
-    object Login : Screen("login")
-    object Survey : Screen("survey")
-    object Results : Screen("results")
-}
--- Mobile/新人任务/第五期/MyApplicationsurvey/app/src/main/java/com/example/myapplicationsurvey/data/json/SurveyJsonExporter.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplicationsurvey.data.json
-
-import kotlinx.serialization.json.Json
-import com.example.myapplicationsurvey.data.model.Survey
-
-object SurveyJsonExporter {
-    fun toJson(survey: Survey): String = Json.encodeToString(Survey.serializer(), survey)
-}
